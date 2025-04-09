9574c3d27016d7537931263e73299fb3c10bffd3
Abhijeet <abhijeetduttaam@gmail.com>
2025-04-09 16:26:23 +0530

    Added Toggle Navabr

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,55 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./header.css";
 
 const Header = () => {
-    return (
-        <header className="header">
-            <nav className="nav container">
-                <a href="index.html" className="nav__logo">Abhijeet</a>
+  const [toggle, setToggle] = useState(false);
 
-                <div className="nav__menu">
-                    <ul className="nav__list grid">
-                        <li className="nav__item">
-                            <a href="#home" className="nav__link">
-                                <i className="uil uil-estate nav__icon"></i> Home
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#about" className="nav__link">
-                                <i className="uil uil-user nav__icon"></i> About
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#skills" className="nav__link">
-                                <i className="uil uil-file-alt nav__icon"></i> Skills
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#services" className="nav__link">
-                                <i className="uil uil-briefcase-alt nav__icon"></i> Services
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#portfolio" className="nav__link">
-                                <i className="uil uil-scenery nav__icon"></i> Portfolio
-                            </a>
-                        </li>
-                        <li className="nav__item">
-                            <a href="#contact" className="nav__link">
-                                <i className="uil uil-message nav__icon"></i> Contact
-                            </a>
-                        </li>
-                    </ul>
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">Abhijeet</a>
 
-                    <i className="uil uil-times nav__close"></i>
-                </div>
+        {/* Mobile menu */}
+        <div className={`nav__menu ${toggle ? "show-menu" : ""}`}>
+          <ul className="nav__list grid">
+            <li className="nav__item">
+              <a href="#home" className="nav__link">
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" className="nav__link">
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" className="nav__link">
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" className="nav__link">
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
 
-                <div className="nav__toggle">
-                    <i className="uil uil-apps"></i>
-                </div>
-            </nav>
-        </header>
-    );
+          {/* Close button */}
+          <i
+            className="uil uil-times nav__close"
+            onClick={() => setToggle(false)}
+          ></i>
+        </div>
+
+        {/* Toggle button */}
+        <div className="nav__toggle" onClick={() => setToggle(true)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
